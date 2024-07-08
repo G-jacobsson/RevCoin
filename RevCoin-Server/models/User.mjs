d4bf0b2f5114058f9581e815e1e6b160024853d9
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-07-08 13:40:23 +0200

    :sparkles: Add mongoose, User model and auth-controller

---
+++ RevCoin-Server/models/User.mjs
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'manager'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordTokenExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.validatePassword = async function (passwordToCheck) {
+  return await bcrypt.compare(passwordToCheck, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_TTL,
+  });
+};
+
+userSchema.methods.createResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.resetPasswordTokenExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+export default mongoose.model('User', userSchema);
